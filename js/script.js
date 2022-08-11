e46fa176cc819fb52e1893c326286e4b37150ac7
Souvik Saha <souviksaha871@gmail.com>
2022-08-11 23:09:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.onclick = () =>{
+  themeToggler.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  themeToggler.classList.remove('active');
+}
+
 
