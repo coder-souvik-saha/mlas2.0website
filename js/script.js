ee3f10c28f1e79ecd946db49107a34bd0b037603
Souvik Saha <souviksaha871@gmail.com>
2022-08-12 17:57:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,4 +19,11 @@ window.onscroll = () =>{
   themeToggler.classList.remove('active');
 }
 
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+  
+  btn.onclick = () =>{
+    let color = btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
 
+});
